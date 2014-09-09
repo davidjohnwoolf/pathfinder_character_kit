894560ae37669a36860ce99ee5a6959540142ddc
David Woolf <davidjohnwoolf@gmail.com>
2014-09-09 16:59:20 -0600

    add characters controller crud actions

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,47 @@
 class CharactersController < ApplicationController
+
+  def index
+    @characters = Character.all
+  end
+
+  def show
+    @character = Character.find(params[:id])
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.new(character_params)
+    if @character.save
+      redirect_to character_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @character = Character.find(params[:id])
+  end
+
+  def update
+    if @character.update_attribute(character_params)
+      redirect_to character_path(@character.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @character.destroy
+    redirect_to characters_path
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :alignment, :deity, :size, :gender, :age, :height, :weight, :skin, :hair, :eyes)
+  end
+
 end
