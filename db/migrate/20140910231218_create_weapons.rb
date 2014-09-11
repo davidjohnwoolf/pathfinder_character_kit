51e531154b4bcb393772b27bfc59e04f9e03b815
David Woolf <davidjohnwoolf@gmail.com>
2014-09-11 16:07:25 -0600

    remove null false on weapon name in weapon migration, was creating error

--- db/migrate/20140910231218_create_weapons.rb
+++ db/migrate/20140910231218_create_weapons.rb
@@ -2,7 +2,7 @@ class CreateWeapons < ActiveRecord::Migration
   def change
     create_table :weapons do |t|
       t.belongs_to :character
-      t.string :name, null: false
+      t.string :name
       t.text :description
       t.string :weapon_class
       t.integer :cost
