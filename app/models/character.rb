df7cd858119b84f70971d36a9889f0dccb8fcde7
David Woolf <davidjohnwoolf@gmail.com>
2014-09-11 21:22:45 -0600

    add allow destroy to character model for weapons nested form

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,5 @@
 class Character < ActiveRecord::Base
   belongs_to :user
   has_many :weapons, :dependent => :destroy
-  accepts_nested_attributes_for :weapons
+  accepts_nested_attributes_for :weapons, :allow_destroy => true
 end
