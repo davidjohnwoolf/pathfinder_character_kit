f21b69742eafc6853cfd9d3aa956abbc36232a72
David Woolf <davidjohnwoolf@gmail.com>
2014-09-12 21:16:38 -0600

    add test for show action in character controller spec

--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe CharactersController, :type => :controller do
 
-  describe '#index', focus: true do
+  describe '#index' do
     before do
       @user = create(:user)
       @characters = create_list(:character, 3, user: @user)
@@ -13,6 +13,20 @@ RSpec.describe CharactersController, :type => :controller do
       expect(response).to be_success
       expect(assigns(:characters).count).to eq 3
       expect(response).to render_template('index')
+    end
+  end
+
+  describe '#show', focus: true do
+    before do
+      @user = create(:user)
+      @character = create(:character, name: 'David', user: @user)
+      sign_in @user
+    end
+    it 'displays an individual character' do
+      get :show, id: @character.id
+      expect(response).to be_success
+      expect(assigns(:character).name).to eq 'David'
+      expect(response).to render_template('show')
     end
   end
 
