f439e49069d37fe1c4dbb00e8e5270507669b7de
David Woolf <davidjohnwoolf@gmail.com>
2014-09-12 19:40:58 -0600

    validate pressence of name in character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,5 @@ class Character < ActiveRecord::Base
   belongs_to :user
   has_many :weapons, :dependent => :destroy
   accepts_nested_attributes_for :weapons, :allow_destroy => true
+  validates_presence_of :name
 end
