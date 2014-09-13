5fe20b660b62013868d9546cae9a222cc2010478
David Woolf <davidjohnwoolf@gmail.com>
2014-09-12 21:11:57 -0600

    add test for index action in characters controller spec

--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe CharactersController, :type => :controller do
 
+  describe '#index', focus: true do
+    before do
+      @user = create(:user)
+      @characters = create_list(:character, 3, user: @user)
+      sign_in @user
+    end
+    it 'displays a collection of characters' do
+      get :index
+      expect(response).to be_success
+      expect(assigns(:characters).count).to eq 3
+      expect(response).to render_template('index')
+    end
+  end
+
 end
