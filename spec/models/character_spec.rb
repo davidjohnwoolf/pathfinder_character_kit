b0a20c22f8321ebbb5bbed1691adb1bd92c5e390
David Woolf <davidjohnwoolf@gmail.com>
2014-09-12 20:55:22 -0600

    create tests for validations and associations for character model spec

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Character, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'associations' do
+    it { should belong_to :user }
+    it { should have_many :weapons }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+  end
+
 end
