c84129d97aa982146f287df7add87273a6de90aa
David Woolf <davidjohnwoolf@gmail.com>
2014-09-12 21:22:45 -0600

    add signed in and not signed in contexts for index and show action tests in character controller spec

--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -6,13 +6,24 @@ RSpec.describe CharactersController, :type => :controller do
     before do
       @user = create(:user)
       @characters = create_list(:character, 3, user: @user)
-      sign_in @user
     end
-    it 'displays a collection of characters' do
-      get :index
-      expect(response).to be_success
-      expect(assigns(:characters).count).to eq 3
-      expect(response).to render_template('index')
+    context 'user signed in' do
+      before do
+        sign_in @user
+      end
+      it 'displays a collection of characters' do
+        get :index
+        expect(response).to be_success
+        expect(assigns(:characters).count).to eq 3
+        expect(response).to render_template('index')
+      end
+    end
+    context 'user is not signed in' do
+      it 'redirects to sign_in' do
+        get :index
+        expect(response).to be_redirect
+        expect(response).to redirect_to new_user_session_path
+      end
     end
   end
 
@@ -20,13 +31,24 @@ RSpec.describe CharactersController, :type => :controller do
     before do
       @user = create(:user)
       @character = create(:character, name: 'David', user: @user)
-      sign_in @user
     end
-    it 'displays an individual character' do
-      get :show, id: @character.id
-      expect(response).to be_success
-      expect(assigns(:character).name).to eq 'David'
-      expect(response).to render_template('show')
+    context 'user signed in' do
+      before do
+        sign_in @user
+      end
+      it 'displays an individual character' do
+        get :show, id: @character.id
+        expect(response).to be_success
+        expect(assigns(:character).name).to eq 'David'
+        expect(response).to render_template('show')
+      end
+    end
+    context 'user is not signed in' do
+      it 'redirects to sign_in' do
+        get :show, id: @character.id
+        expect(response).to be_redirect
+        expect(response).to redirect_to new_user_session_path
+      end
     end
   end
 
